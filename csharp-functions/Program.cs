f0710c776f432433f8878635085a58a4869193ea
DorinVieru <dorino.vieru@gmail.com>
2024-04-18 15:14:04 +0200

    UP: Complete first function

--- csharp-functions/Program.cs
+++ csharp-functions/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace csharp_functions
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace csharp_functions
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // FUNZIONE PER STAMPARE A VIDEO UN ARRAY
+            {
+                void StampaArray(int[] array)
+                {
+                    Console.Write("[");
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        Console.Write(array[i]);
+                        if (i < array.Length - 1)
+                            Console.Write(", ");
+                    }
+                    Console.WriteLine("]");
+                }
+                int[] elements = { 1, 2, 3, 4, 5, 6, 7 };
+                StampaArray(elements);
+            }
+
+            
+            
         }
     }
 }
